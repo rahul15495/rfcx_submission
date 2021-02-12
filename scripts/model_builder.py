c55dacc5d32bbf8c48fee7b8098d70c87c1f40ae
rahul15495 <rahul.8d@gmail.com>
2021-02-12 15:40:49 +0000

    single batch run successful

--- scripts/model_builder.py
+++ scripts/model_builder.py
@@ -24,26 +24,13 @@ class Upstream_Model(torch.nn.Module):
         
         if sequence_length> self.maxlen:
             
-            sub_sequences= []
-            num_subseq= sequence_length//self.maxlen
-            start= 0
+            split_size = list(range(self.maxlen,sequence_length, self.maxlen))
+            rem = sequence_length % self.maxlen
+            if rem:
+                split_size += [rem]
+
+            return torch.split(inp, split_size, dim=1)
             
-            for i in range(1,num_subseq+1):
-                end= self.maxlen*i
-                if axes_length==2:
-                    sub_sequences.append(inp[:, start:end])
-                else:
-                    sub_sequences.append(inp[:, start:end, :])
-                
-                start=end
-                
-            if end<sequence_length:
-                if axes_length==2:
-                    sub_sequences.append(inp[:, start:])
-                else:
-                    sub_sequences.append(inp[:, start:, :])
-        
-            return sub_sequences
         else:
             return [inp]
             
@@ -75,42 +62,31 @@ class Upstream_Model(torch.nn.Module):
 class Downstream_Model(nn.Module):
     '''
     Takes input shape of (Batch_Size, Encoded_layers, Time_step , Hidden_Size_Of_Upstream)
-    for eg: (1,3,4801, 768)
+    for eg: (1,4801, 768,3)
     '''
     
-    def __init__(self, encoder_num_layers=3, frame_length=4801, hidden_layers=768, num_classes=24):
-        
-        super(Downstream_Model,self).__init__()
+    def __init__(self,encoder_num_layers=3, input_dim=768, hidden_dim=128, class_num=24):
+        super(Downstream_Model, self).__init__()
         
         self.num_layers  = encoder_num_layers
         self.weight      = nn.Parameter(torch.ones(self.num_layers) / self.num_layers)
-        self.drop1        = nn.Dropout()
-        self.pool        = nn.AvgPool2d(kernel_size= (frame_length, 1))
-        self.drop2       = nn.Dropout()
-        self.hidden1     = nn.Linear(in_features= hidden_layers, out_features =128)
-        self.act1        = nn.ReLU()
-        self.drop3       = nn.Dropout()
-        self.logits      = nn.Linear(in_features= 128, out_features =num_classes)
-        self.classifier   = nn.Sigmoid()
         
+        self.rnn = nn.GRU(input_size=input_dim, hidden_size=hidden_dim, num_layers=1, dropout=0.3,
+                          batch_first=True, bidirectional=False)
+
+        self.out = nn.Linear(hidden_dim, class_num)
+        self.out_fn = nn.Sigmoid()
+
     def forward(self, x):
-        
         #weighted_sum. expected_output (Batch_Size, Time_step , Hidden_Size_Of_Upstream)
         softmax_weight = nn.functional.softmax(self.weight, dim=-1)
         x_weighted_sum = torch.matmul(x,softmax_weight)
-        x_weighted_sum = self.drop1(x_weighted_sum)
         
-        # frame-wise average pool. expected output (Batch_Size, 1 , Hidden_Size_Of_Upstream) 
-        golbal_activations = self.pool(x_weighted_sum)
-        golbal_activations = golbal_activations.squeeze(dim=1) #(Batch_Size, Hidden_Size_Of_Upstream) 
-        golbal_activations = self.drop2(golbal_activations)
-        
-        hidden_layer = self.hidden1(golbal_activations)
-        hidden_layer = self.act1(hidden_layer)
-        hidden_layer = self.drop3(hidden_layer)
-        
-        logits_layer = self.logits(hidden_layer)
-        return self.classifier(logits_layer)
+        _, h_n = self.rnn(x_weighted_sum)
+        hidden = h_n[-1, :, :]
+        logits = self.out(hidden)
+        result = self.out_fn(logits)
+        return result
     
 class Wrapper_Model(torch.nn.Module):
     def __init__(self, base_transformer_model):
